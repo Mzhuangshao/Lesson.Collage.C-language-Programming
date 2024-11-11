12b00e2d8724e647f162852f3310ce1e9592a421
M_zhuangshao <79238849+Mzhuangshao@users.noreply.github.com>
2024-11-11 11:20:50 +0800

    class

---
+++ 00_work/01command.c
@@ -0,0 +1,17 @@
+//显示梯形
+#include <stdio.h>
+
+void main()
+{
+    int count, number, scanNumber;
+    printf("Enter Number\n");
+    scanf("%d",&scanNumber);
+    for(count = 0; count <= scanNumber; count ++)
+    {
+        for(number = 0; number <= count; number ++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}

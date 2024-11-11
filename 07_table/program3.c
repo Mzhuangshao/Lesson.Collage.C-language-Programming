df2b9eadda420a0988898ba3ecbc35af78ccfc75
M_zhuangshao <79238849+Mzhuangshao@users.noreply.github.com>
2024-11-11 17:30:05 +0800

    upload

--- 07_table/program3.c
+++ 07_table/program3.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+void main()
+{
+    int peach[10] = {1};
+    for (int number = 1; number <= 10; number++)
+    {
+        peach[number] = (peach[number - 1] + 1) * 2;
+    }
+    for (int day = 10; day >= 1; day--)
+    {
+        printf("第%d天有%d个桃子\n", day, peach[day - 1]);
+    }
+}

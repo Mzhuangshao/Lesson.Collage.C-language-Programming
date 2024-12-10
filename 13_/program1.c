4d1404754fcbda93b57f03b88e06d7d921430e84
M_zhuangshao <79238849+Mzhuangshao@users.noreply.github.com>
2024-12-10 16:09:06 +0800

    class

--- 13_/program1.c
+++ 13_/program1.c
@@ -1,1 +1,18 @@
 #include <stdio.h>
+int x1 = 30, x2 = 40;
+void swap(int x, int y)
+{
+    x1 = x;
+    x = y;
+    y = x1;
+}
+
+void main()
+{
+    int x3 = 10, x4 = 20;
+    printf("%d %d %d %d\n", x1, x2, x3, x4);
+    swap(x3, x4);
+    printf("%d %d %d %d\n", x1, x2, x3, x4);
+    swap(x1, x2);
+    printf("%d %d %d %d\n", x1, x2, x3, x4);
+}
--- 13_/program2.c
+++ 13_/program2.c
@@ -1,1 +1,25 @@
 #include <stdio.h>
+
+int A;
+
+void main()
+{
+    extern int power(int n);
+    int b = 3, c, d, m;
+    printf("Enter A , m : ");
+    scanf("%d %d", &A, &m);
+    c = A * b;
+    printf("\n%d * %d = %d\n", A, b, c);
+    d = power(m);
+    printf("%d * %d = %d", A, m, d);
+}
+
+int power(int n)
+{
+    int i,y = 1;
+    for (i = 1; i <= n; i++)
+    {
+        y * A;
+    }
+    return y;
+}

77ea3530b8441051f22263d191f460b8f71cba49
M_zhuangshao <79238849+Mzhuangshao@users.noreply.github.com>
2024-12-24 16:53:48 +0800

    class

--- 16_final/program1.c
+++ 16_final/program1.c
@@ -17,43 +17,40 @@ void Q13();
 void Q14();
 void Q15();
 void Q16();
-void Q17();
-void Q18();
-void Q19();
 
 void main()
 {
-    printf("\n-------\n");
-    Q1();
-    printf("\n-------\n");
-    Q2();
-    printf("\n-------\n");
-    Q4();
-    printf("\n-------\n");
-    Q5();
-    printf("\n-------\n");
-    Q6();
+    // printf("\n-------\n");
+    // Q1();
+    // printf("\n-------\n");
+    // Q2();
+    // printf("\n-------\n");
+    // Q4();
+    // printf("\n-------\n");
+    // Q5();
+    // printf("\n-------\n");
+    // Q6();
     printf("\n-------\n");
     Q7();
     printf("\n-------\n");
-    Q8();
-    printf("\n-------\n");
-    Q9();
-    printf("\n-------\n");
-    Q10();
-    printf("\n-------\n");
-    Q11();
-    printf("\n-------\n");
-    Q12();
-    printf("\n-------\n");
-    Q13();
-    printf("\n-------\n");
-    Q14();
-    printf("\n-------\n");
-    Q15();
-    printf("\n-------\n");
-    Q16();
-    printf("\n-------\n");
+    // Q8();
+    // printf("\n-------\n");
+    // Q9();
+    // printf("\n-------\n");
+    // Q10();
+    // printf("\n-------\n");
+    // Q11();
+    // printf("\n-------\n");
+    // Q12();
+    // printf("\n-------\n");
+    // Q13();
+    // printf("\n-------\n");
+    // Q14();
+    // printf("\n-------\n");
+    // Q15();
+    // printf("\n-------\n");
+    // Q16();
+    // printf("\n-------\n");
 }
 
 void Q1()
@@ -92,27 +89,31 @@ void Q5()
     // scanf("%.2f", &h);
     printf("\n圆周长是%.2f  圆面积是%.2f  球表面积是%.2f  球体积是%.2f  圆柱体积是%.2f  ", Circumference, CircleArea, BallSurfaceArea, BallVolume, CylinderVolume);
 }
+
 void Q6()
 {
-    int a = 3, b = 4, c = 5;
+    int a = 3, b = 4, c = 5, u = 51241, n = 128765;
     float x = 1.2, y = 2.4, z = -3.6;
-    long u = 51241, n = 128765;
     char c1 = 'a', c2 = 'b';
-    int ac1 = c1, ac2 = c2;
     printf("a= %d  b= %d  c= %d\n", a, b, c);
     printf("x=%.6f,y=%.6f,z=%.6f\n", x, y, z);
     printf("x+y= %.2f  y+z=%.2f  z+x=%.2f\n", x + y, y + z, z + x);
     printf("u= %d  n=   %d\n", u, n);
-    printf("c1=\'%c\' or %d\n", c1, ac1);
-    printf("c2=\'%c\' or %d\n", c2, ac2);
+    printf("c1=\'%c\' or %d(ASCII)\n", c1, c1);
+    printf("c2=\'%c\' or %d(ASCII)\n", c2, c2);
 }
+
 void Q7()
 {
-    printf("*****\n");
-    printf("*    \n");
-    printf("*    \n");
-    printf("*****\n");
+    for (int layer = 0; layer < 7; layer++)
+    {
+        if (layer == 0 || layer == 3 || layer == 6) //  当layer等于0或等于3时
+            printf("*****\n");
+        else
+            printf("*    \n");
+    }
 }
+
 void Q8()
 {
     int ScanNumber, y1 = 0, y2 = 0;

59dbf2b767f7f8c2acfc1c4f0424b53fd143b1f4
M_zhuangshao <79238849+Mzhuangshao@users.noreply.github.com>
2024-12-26 23:44:51 +0800

    class

--- 00_Only test/command.c
+++ 00_Only test/command.c
@@ -2,12 +2,12 @@
 
 void main()
 {
-    int da = 9037;
+    int da = 8750;
     da = da % 10000;
 
     printf("\nda1 %d", da);
     printf("\n千 %d", da / 1000);       // 千位
-    printf("\n百 %d", da % 1000 / 100); // 百位
-    printf("\n十 %d", da % 100 / 10);   // 十位
+    printf("\n百 %d", (da % 1000) / 100); // 百位
+    printf("\n十 %d", (da % 100) / 10);   // 十位
     printf("\n个 %d", da % 10);         // 个位
 }
